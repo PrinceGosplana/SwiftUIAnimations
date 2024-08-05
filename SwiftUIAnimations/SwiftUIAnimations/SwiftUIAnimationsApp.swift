159f7aa939bd100bdc596cca99a73fde3c5d2cc6
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-05 13:50:48 +0300

    init the project

---
+++ SwiftUIAnimations/SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Oleksandr Isaiev on 05.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
