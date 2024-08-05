c5faacb5d5777a6be3086246166d13a538b35741
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-05 15:01:30 +0300

    work with ui

--- SwiftUIAnimations/SwiftUIAnimations/OnboardingView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/OnboardingView.swift
@@ -9,6 +9,9 @@ import RiveRuntime
 import SwiftUI
 
 struct OnboardingView: View {
+
+    let button = RiveViewModel(fileName: "button")
+
     var body: some View {
         ZStack {
             RiveViewModel(fileName: "shapes").view()
@@ -20,12 +23,22 @@ struct OnboardingView: View {
                         .offset(x: 200, y: 100)
                 }
 
-            RiveViewModel(fileName: "button").view()
+            button.view()
                 .frame(width: 236, height: 64)
                 .overlay {
                     Label("Start the course", systemImage: "arrow.forward")
                         .offset(x: 4, y: 4)
                         .font(.headline)
+                }
+                .background {
+                    Color.black
+                        .clipShape(.rect(cornerRadius: 30))
+                        .blur(radius: 10)
+                        .opacity(0.3)
+                        .offset(y: 10.0)
+                }
+                .onTapGesture {
+                    button.play(animationName: "active")
                 }
         }
     }
