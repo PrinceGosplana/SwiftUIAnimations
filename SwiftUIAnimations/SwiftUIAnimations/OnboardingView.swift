0c5aa26a5851c34a9c57410629f21c8f6378f72a
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-06 12:01:27 +0300

    done with ui for onboarding screen

--- SwiftUIAnimations/SwiftUIAnimations/OnboardingView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/OnboardingView.swift
@@ -16,7 +16,7 @@ struct OnboardingView: View {
         ZStack {
             animatedBackground
 
-            VStack {
+            VStack(alignment: .leading, spacing: 16) {
 
                 Text("Learn design $ code")
                     .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
@@ -24,6 +24,10 @@ struct OnboardingView: View {
 
                 Text("Don't skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.")
                     .customFont(.body)
+                    .opacity(0.7)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                Spacer(minLength: 0)
 
                 button.view()
                     .frame(width: 236, height: 64)
@@ -42,7 +46,13 @@ struct OnboardingView: View {
                     .onTapGesture {
                         button.play(animationName: "active")
                 }
+
+                Text("Purchase includes access to 30+ courses, 240+ premium tutorials, 120+ hours of videos, source files and certificates.")
+                    .customFont(.footnote)
+                    .opacity(0.7)
             }
+            .padding(40)
+            .padding(.top, 40)
         }
     }
 
