bb3903193adeaf76f1ceee50050b4791c47d766b
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-09 11:14:13 +0300

    implement TabBarView

--- SwiftUIAnimations/SwiftUIAnimations/Views/TabBarView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/Views/TabBarView.swift
@@ -13,17 +13,33 @@ struct TabBarView: View {
     private let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
 
     var body: some View {
-        HStack {
+        VStack {
+            Spacer(minLength: 0)
+            HStack {
+                content
+            }
+            .padding(12)
+            .background(Color(.background2).opacity(0.8))
+            .background(.ultraThinMaterial)
+            .mask(RoundedRectangle(cornerRadius: 24, style: .continuous))
+            .shadow(color: Color(.background2).opacity(0.3), radius: 20, x: 0, y: 20)
+            .overlay(RoundedRectangle(cornerRadius: 24, style: .continuous).stroke(.linearGradient(colors: [.white.opacity(0.5), .white.opacity(0)], startPoint: .topLeading, endPoint: .bottomTrailing)))
+            .padding(.horizontal, 24)
+        }
+    }
+
+    var content: some View {
+        ForEach(TabItem.tabItems) { item in
             Button {
-                try? icon.setInput("active", value: true)
+                try? item.icon.setInput("active", value: true)
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    try? icon.setInput("active", value: false)
+                    try? item.icon.setInput("active", value: false)
                 }
             } label: {
-                icon.view()
+                item.icon.view()
+                    .frame(height: 36)
             }
         }
-        .background(Color(.background2).opacity(0.8))
     }
 }
 
