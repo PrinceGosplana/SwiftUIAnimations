70be679778fdde037f502e8ef375c4cec578d311
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-09 12:17:56 +0300

    add animation during selection

--- SwiftUIAnimations/SwiftUIAnimations/Views/TabBarView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/Views/TabBarView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct TabBarView: View {
 
+    @State var selectedTab: Tab = .chat
     private let icon = RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")
+
+    /// Animation
+    @Namespace private var animation
 
     var body: some View {
         VStack {
@@ -35,9 +39,22 @@ struct TabBarView: View {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                     try? item.icon.setInput("active", value: false)
                 }
+                withAnimation {
+                    selectedTab = item.tab
+                }
             } label: {
                 item.icon.view()
                     .frame(height: 36)
+                    .opacity(selectedTab == item.tab ? 1 : 0.5)
+                    .background {
+                        if selectedTab == item.tab {
+                            RoundedRectangle(cornerRadius: 2)
+                                .fill(Color.accentColor)
+                                .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
+                                .frame(width: 20, height: 4)
+                                .offset(y: -20)
+                        }
+                    }
             }
         }
     }
