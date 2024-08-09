9b363f1aaa8155f7bd42fbd3d0aca90c8c39c95e
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-09 12:17:37 +0300

    add tab enum and extent tabItem

---
+++ SwiftUIAnimations/SwiftUIAnimations/Tab.swift
@@ -0,0 +1,12 @@
+//
+//  Tab.swift
+//  SwiftUIAnimations
+//
+//  Created by Oleksandr Isaiev on 09.08.2024.
+//
+
+import Foundation
+
+enum Tab: String {
+    case chat, search, timer, bell, user
+}
--- SwiftUIAnimations/SwiftUIAnimations/TabItem.swift
+++ SwiftUIAnimations/SwiftUIAnimations/TabItem.swift
@@ -11,14 +11,16 @@ import SwiftUI
 struct TabItem: Identifiable {
     var id = UUID()
     var icon: RiveViewModel
+    var tab: Tab
 }
 
 extension TabItem {
     static let tabItems = [
-        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT")),
-        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH")),
-        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER")),
-        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL")),
-        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "USER_Interactivity", artboardName: "USER"))
+        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "CHAT_Interactivity", artboardName: "CHAT"), tab: .chat),
+        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "SEARCH_Interactivity", artboardName: "SEARCH"), tab: .search),
+        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "TIMER_Interactivity", artboardName: "TIMER"), tab: .timer),
+        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "BELL_Interactivity", artboardName: "BELL"), tab: .bell),
+        TabItem(icon: RiveViewModel(fileName: "icons", stateMachineName: "USER_Interactivity", artboardName: "USER"), tab: .user)
     ]
 }
+
