5664f2d53c3e00e8c2a44f01e91b5576416a1285
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-09 09:06:27 +0300

    add check animation

--- SwiftUIAnimations/SwiftUIAnimations/SignInView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/SignInView.swift
@@ -5,12 +5,14 @@
 //  Created by Oleksandr Isaiev on 06.08.2024.
 //
 
+import RiveRuntime
 import SwiftUI
 
 struct SignInView: View {
 
     @State var email = ""
     @State var password = ""
+    let check = RiveViewModel(fileName: "check", stateMachineName: "State Machine 1")
 
     var body: some View {
         VStack(spacing: 24) {
@@ -23,7 +25,7 @@ struct SignInView: View {
             VStack(alignment: .leading, spacing: 0) {
                 Text("Email")
                     .customFont(.headline)
-                .foregroundStyle(.secondary)
+                    .foregroundStyle(.secondary)
 
                 TextField("", text: $email)
                     .customTextField()
@@ -32,21 +34,13 @@ struct SignInView: View {
             VStack(alignment: .leading, spacing: 0) {
                 Text("Password")
                     .customFont(.headline)
-                .foregroundStyle(.secondary)
+                    .foregroundStyle(.secondary)
 
                 SecureField("", text: $password)
                     .customTextField(image: Image(.iconLock))
             }
 
-            Label("Sign In", systemImage: "arrow.right")
-                .customFont(.headline)
-                .padding(20)
-                .frame(maxWidth: .infinity)
-                .background(Color(hex: "F77D8E"))
-                .foregroundStyle(.white)
-                .cornerRadius(20, corners: [.topRight, .bottomLeft, .bottomRight])
-                .cornerRadius(8, corners: [.topLeft])
-                .shadow(color: Color(hex: "F77D8E").opacity(0.5), radius: 20, x: 0, y: 10)
+            SignInButton()
 
             HStack {
                 Rectangle().frame(height: 1).opacity(0.1)
@@ -95,6 +89,28 @@ struct SignInView: View {
                 )
         )
         .padding()
+        .overlay {
+            check.view()
+                .frame(width: 100, height: 100)
+                .allowsHitTesting(false)
+        }
+    }
+
+    @ViewBuilder
+    func SignInButton() -> some View {
+        Button {
+            try? check.triggerInput("Check")
+        } label: {
+            Label("Sign In", systemImage: "arrow.right")
+                .customFont(.headline)
+                .padding(20)
+                .frame(maxWidth: .infinity)
+                .background(Color(hex: "F77D8E"))
+                .foregroundStyle(.white)
+                .cornerRadius(20, corners: [.topRight, .bottomLeft, .bottomRight])
+                .cornerRadius(8, corners: [.topLeft])
+                .shadow(color: Color(hex: "F77D8E").opacity(0.5), radius: 20, x: 0, y: 10)
+        }
     }
 }
 
