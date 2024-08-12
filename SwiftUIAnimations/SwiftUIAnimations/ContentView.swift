7961dd682831c493d8d53bea3bd5c38ca42b9128
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-12 10:02:53 +0300

    implement humburger buutton

--- SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
@@ -5,12 +5,14 @@
 //  Created by Oleksandr Isaiev on 05.08.2024.
 //
 
+import RiveRuntime
 import SwiftUI
 
 struct ContentView: View {
 
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
-
+    let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
+    @State private var isMenuOpen = false
     var body: some View {
         ZStack {
             switch selectedTab {
@@ -26,6 +28,17 @@ struct ContentView: View {
                 Text("User")
             }
             
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: Color(.shadow).opacity(0.2), radius: 5, x: 0, y: 5)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    try? button.setInput("isOpen", value: isMenuOpen)
+                    isMenuOpen.toggle()
+                }
+
             TabBarView()
         }
     }
