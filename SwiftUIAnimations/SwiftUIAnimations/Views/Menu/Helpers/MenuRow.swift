efd849af4360c8db78418ac1647742b575af866f
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-13 11:05:16 +0300

    code refactoring

---
+++ SwiftUIAnimations/SwiftUIAnimations/Views/Menu/Helpers/MenuRow.swift
@@ -0,0 +1,46 @@
+//
+//  MenuRow.swift
+//  SwiftUIAnimations
+//
+//  Created by Oleksandr Isaiev on 13.08.2024.
+//
+
+import SwiftUI
+
+struct MenuRow: View {
+    var item: MenuItem
+    @Binding var selectedMenu: SelectedMenu
+
+    var body: some View {
+        HStack(spacing: 14) {
+            item.icon.view()
+                .frame(width: 32, height: 32)
+                .opacity(0.6)
+
+            Text(item.text)
+                .customFont(.headline)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(22)
+        .background(
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .fill(.blue)
+                .frame(maxWidth: selectedMenu == item.menu ? .infinity : 0)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        )
+        .background(Color(.background2))
+        .onTapGesture {
+            try? item.icon.setInput("active", value: true)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                try? item.icon.setInput("active", value: true)
+            }
+            withAnimation {
+                selectedMenu = item.menu
+            }
+        }
+    }
+}
+
+#Preview {
+    MenuRow(item: MenuItem.menuItems[0], selectedMenu: .constant(.home))
+}
