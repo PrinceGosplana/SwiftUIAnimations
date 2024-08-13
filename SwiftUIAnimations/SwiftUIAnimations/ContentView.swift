26a8a7819148b363f74ea7a03d4e9851ab74a593
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-13 12:37:55 +0300

    side menu animation

--- SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
@@ -16,6 +16,13 @@ struct ContentView: View {
     var body: some View {
         ZStack {
 
+            Color(.background2).ignoresSafeArea()
+
+            SideMenu()
+                .opacity(isMenuOpen ? 1 : 0)
+                .offset(x: isMenuOpen ? 0 : -300)
+                .rotation3DEffect(.degrees(isMenuOpen ? 0 : 30), axis: (x: 0, y: 1, z: 0))
+
             Group {
                 switch selectedTab {
                 case .chat:
@@ -36,6 +43,13 @@ struct ContentView: View {
             .safeAreaInset(edge: .top) {
                 Color.clear.frame(height: 104)
             }
+            .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .rotation3DEffect(
+                .degrees(isMenuOpen ? 30 : 0),
+                axis: (x: 0.0, y: -1.0, z: 0.0)
+            )
+            .offset(x: isMenuOpen ? 265 : 0)
+            .scaleEffect(isMenuOpen ? 0.9 : 1)
             .ignoresSafeArea()
 
 
@@ -45,12 +59,17 @@ struct ContentView: View {
                 .shadow(color: Color(.shadow).opacity(0.2), radius: 5, x: 0, y: 5)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .padding()
+
+                .offset(x: isMenuOpen ? 216 : 0)
                 .onTapGesture {
                     try? button.setInput("isOpen", value: isMenuOpen)
-                    isMenuOpen.toggle()
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7)) {
+                        isMenuOpen.toggle()
+                    }
                 }
 
             TabBarView()
+                .offset(y: isMenuOpen ? 300 : 0)
         }
     }
 }
